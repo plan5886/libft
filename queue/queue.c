18645455779b20d3192d6c441ff9a50f2335ece6
Myeonghyeon Park <mypark@c1r2s5.42seoul.kr>
2022-03-17 21:08:03 +0900

    q->tail

--- queue/queue.c
+++ queue/queue.c
@@ -6,7 +6,7 @@
 /*   By: mypark <mypark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 02:48:54 by mypark            #+#    #+#             */
-/*   Updated: 2022/03/15 17:01:57 by mypark           ###   ########.fr       */
+/*   Updated: 2022/03/17 21:07:34 by mypark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,17 @@
 
 static t_queue_node	*pop(struct s_queue *q)
 {
-	return (cir_lstpop_front(&q->head));
+	t_queue_node	*node;
+
+	node = cir_lstpop_front(&q->head);
+	q->tail = q->head->prev;
+	return (node);
 }
 
 static void	push(t_queue *q, t_queue_node *new)
 {
 	cir_lstadd_back(q->head, new);
+	q->tail = q->head->prev;
 }
 
 t_queue_node	*new_queue_node(void *content)
